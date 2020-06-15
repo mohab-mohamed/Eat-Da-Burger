ac3f67551b3fa4678a7f69cadf16b62e42854a22
Mohab <mohab8m@gmail.com>
2020-06-15 01:29:22 -0400

    backend functionality, models/controllers

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ramen Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Protein Burger', false);
